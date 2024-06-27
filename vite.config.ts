6bfa4fdd214c384ff5862e23795eea232351d90c
illia-st <illiastetsenko@knu.ua>
2024-06-27 10:11:29 +0300

    moved to a build directory

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build',
+  },
   test: {
     globals: true,
     environment: 'jsdom',
