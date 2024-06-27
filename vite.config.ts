ce3a9298d0e3eaa1e5d9f8c6db35f957bb858421
illia-st <illiastetsenko@knu.ua>
2024-06-27 11:29:38 +0300

    try to fix

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: './',
-  base: '/retreat-web-site/',
+  base: './',
   plugins: [react()],
+  publicDir: 'public',
   build: {
     outDir: 'build',
   },
