82fb5c3f1a78df0d7a1f66cee3748e0793c59a70
illia-st <illiastetsenko@knu.ua>
2025-01-12 19:46:05 +0200

    complete basic info

--- src/components/BasicInfo.tsx
+++ src/components/BasicInfo.tsx
@@ -1,50 +1,123 @@
 /* eslint-disable react/no-array-index-key */
 import React from 'react';
 import {
-  // Grid,
   Typography,
   Box,
   Accordion,
   AccordionSummary,
   AccordionDetails,
   Container,
+  List,
+  ListItem,
+  ListItemText,
 } from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-// import EmblaCarouselSimple from './caruosels/Simple';
 
 const sections = [
   {
     title: 'Why Our Team?',
-    content: `• Improve your physical strength, flexibility, and overall fitness.\n
-      • Build greater self-discipline.\n
-      • Develop a deeper awareness of your thoughts and feelings.\n
-      • Connect more profoundly with yourself.\n
-      • Release tension and emotional blocks.\n
-      • Cultivate greater self-love.\n
-      • Discover how to feel joy simply for being.  
-    `,
+    content: [
+      {
+        header: '• Authentic Traditional Yoga:',
+        description:
+          '    We approach yoga as a complete system that works on all layers of your being, guiding you toward Chitta Vritti Nirodhi — the stillness of the mind.',
+      },
+      {
+        header: '• Individual Approach:',
+        description:
+          '    We tailor the experience to your personal journey, ensuring our practices support your unique path to balance and growth.',
+      },
+      {
+        header: '• Two Teachers.',
+        description:
+          '    You will be supported by two experienced yoga teachers, allowing us to observe and guide you more deeply throughout your retreat experience.',
+      },
+      {
+        header: '• We Don’t Push, We Suggest.',
+        description:
+          '    We offer a variety of practices, but you are free to choose what resonates most with you — no pressure, just guidance.',
+      },
+      {
+        header: '• Heartfelt Atmosphere.',
+        description:
+          '    We create a warm, supportive environment where you can feel truly at ease and present with your practice.',
+      },
+      {
+        header: '• Diverse Techniques & Tools.',
+        description:
+          '    Experience a range of methods and tools to discover what best helps you on your journey — with a chance to explore different practices during the retreat.',
+      },
+    ],
   },
   {
     title: 'What will you get from the retreat?',
-    content: `• Improve your physical strength, flexibility, and overall fitness.\n
-      • Build greater self-discipline.\n
-      • Develop a deeper awareness of your thoughts and feelings.\n
-      • Connect more profoundly with yourself.\n
-      • Release tension and emotional blocks.\n
-      • Cultivate greater self-love.\n
-      • Discover how to feel joy simply for being.  
-    `,
+    content: [
+      {
+        header: '• Physical Fitness',
+        description:
+          '    Improve your physical strength, flexibility, and overall fitness.',
+      },
+      {
+        header: '• Self-Discipline',
+        description: '    Build greater self-discipline.',
+      },
+      {
+        header: '• Awareness',
+        description:
+          '    Develop a deeper awareness of your thoughts and feelings.',
+      },
+      {
+        header: '• Self-Connection',
+        description: '    Connect more profoundly with yourself.',
+      },
+      {
+        header: '• Emotional Release',
+        description: '    Release tension and emotional blocks.',
+      },
+      {
+        header: '• Self-Love',
+        description: '    Cultivate greater self-love.',
+      },
+      {
+        header: '• Joyful Living',
+        description: '    Discover how to feel joy simply for being.',
+      },
+    ],
   },
   {
-    title: 'Who are our retreats for? ',
-    content: `• For those who want to dive deeper into the world of yoga and gain new experience.\n
-      • For those seeking to improve their yoga practice and make it more consistent.\n
-      • For those familiar with Chaturanga and eager to explore the full potential of their body.\n
-      • For those feeling stuck in the everyday routine and looking for a fresh dose of inspiration and energy.\n
-      • For those who want to connect with a new circle of like-minded people.\n
-      • For those aiming to enter a resourceful state where life feels fulfilling and creative. \n
-      • And for those who simply want to relax, discover something new, and see life from a different perspective.
-    `,
+    title: 'Who are our retreats for?',
+    content: [
+      {
+        header: '• Yoga Enthusiasts',
+        description:
+          '    For those who want to dive deeper into the world of yoga.',
+      },
+      {
+        header: '• Improved Practice',
+        description:
+          '    For those seeking to make their yoga practice more consistent.',
+      },
+      {
+        header: '• Chaturanga Explorers',
+        description:
+          '    For those familiar with Chaturanga and eager to explore their potential.',
+      },
+      {
+        header: '• Routine Breakers',
+        description:
+          '    For those stuck in a routine and seeking inspiration.',
+      },
+      {
+        header: '• Community Seekers',
+        description:
+          '    For those wanting to connect with like-minded people.',
+      },
+      {
+        header: '• Relaxation Lovers',
+        description:
+          '    For those who simply want to relax and see life from a fresh perspective.',
+      },
+    ],
   },
 ];
 
@@ -78,24 +151,47 @@ function BasicInfo() {
           >
             <AccordionSummary expandIcon={<ExpandMoreIcon />}>
               <Typography
-                variant="h6"
-                sx={{ color: 'primary.contrastText', fontSize: '1.75rem' }}
+                variant="h5"
+                sx={{ color: 'primary.contrastText', fontSize: '2rem' }}
               >
                 {section.title}
               </Typography>
             </AccordionSummary>
             <AccordionDetails>
-              <Typography
-                variant="body1"
-                sx={{
-                  whiteSpace: 'pre-line',
-                  fontSize: '1.6rem',
-                  lineHeight: 1.6,
-                  color: 'primary.contrastText',
-                }}
-              >
-                {section.content}
-              </Typography>
+              <List>
+                {section.content.map((item, subIndex) => (
+                  <ListItem
+                    key={subIndex}
+                    sx={{
+                      display: 'flex',
+                      flexDirection: 'column',
+                      alignItems: 'flex-start',
+                      marginBottom: 2,
+                    }}
+                  >
+                    <Typography
+                      variant="h6"
+                      sx={{
+                        fontSize: '1.5rem',
+                        fontWeight: 'bold',
+                        color: 'primary.contrastText',
+                      }}
+                    >
+                      {item.header}
+                    </Typography>
+                    <ListItemText
+                      primary={item.description}
+                      primaryTypographyProps={{
+                        sx: {
+                          fontSize: '1.4rem',
+                          lineHeight: 1.7,
+                          color: 'primary.contrastText',
+                        },
+                      }}
+                    />
+                  </ListItem>
+                ))}
+              </List>
             </AccordionDetails>
           </Accordion>
         </Container>
